94070d884a0b4b7277d8629d0394c07226af43dc
guavalines <konstrictor@gmail.com>
2022-12-20 23:20:19 -0800

    added more cards

--- app.js
+++ app.js
@@ -23,4 +23,28 @@ const cardArray = [
     name: 'pizza',
     img: 'images/pizza.png'
   },
+  {
+    name: 'fries',
+    img: 'images/fries.png'
+  },
+  {
+    name: 'cheeseburger',
+    img: 'images/cheeseburger.png'
+  },
+  {
+    name: 'hotdog',
+    img: 'images/hotdog.png'
+  },
+  {
+    name: 'ice-cream',
+    img: 'images/ice-cream.png'
+  },
+  {
+    name: 'milkshake',
+    img: 'images/milkshake.png'
+  },
+  {
+    name: 'pizza',
+    img: 'images/pizza.png'
+  }
 ]
